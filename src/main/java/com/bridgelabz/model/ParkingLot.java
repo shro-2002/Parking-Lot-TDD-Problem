0ab484bc771c711c0e4832d8f46dec60dbfd89f4
shro-2002 <96790181+shro-2002@users.noreply.github.com>
2023-12-21 15:41:00 +0530

    Added UC-2 and TC-2: Unpark Cars from Parking Lot

--- src/main/java/com/bridgelabz/model/ParkingLot.java
+++ src/main/java/com/bridgelabz/model/ParkingLot.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.model;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class ParkingLot {
@@ -13,9 +14,25 @@ public class ParkingLot {
 	}
 
 	public void parkCar(Car car) {
-		parkedCars.add(car);
-		System.out.println(car.getLicensePlate() + " has been parked.");
+		if (parkedCars.size() < capacity) {
+			parkedCars.add(car);
+			System.out.println(car.getLicensePlate() + " has been parked.");
+		} else {
+			System.out.println("Parking lot is full. Cannot park " + car.getLicensePlate() + ".");
+		}
+	}
 
+	public void unparkCar(String licensePlate) {
+		Iterator<Car> iterator = parkedCars.iterator();
+		while (iterator.hasNext()) {
+			Car car = iterator.next();
+			if (car.getLicensePlate().equals(licensePlate)) {
+				iterator.remove();
+				System.out.println(car.getLicensePlate() + " has been unparked.");
+				return;
+			}
+		}
+		System.out.println("Car with license plate " + licensePlate + " not found in the parking lot.");
 	}
 
 	public List<Car> getParkedCars() {
@@ -23,6 +40,7 @@ public class ParkingLot {
 	}
 
 	public void printParkedCars() {
+		System.out.println();
 		System.out.println("Parked cars: ");
 		for (Car car : parkedCars) {
 			System.out.println(car.getLicensePlate());
--- src/test/java/com/bridgelabz/parkinglottest/ParkingLostTest.java
+++ src/test/java/com/bridgelabz/parkinglottest/ParkingLostTest.java
@@ -10,24 +10,38 @@ import com.bridgelabz.model.ParkingLot;
 
 class ParkingLostTest {
 	ParkingLot parkingLot;
+	Car car1;
+	Car car2;
 
 	@BeforeEach
 	void setUp() throws Exception {
 		parkingLot = new ParkingLot(100);
+		car1 = new Car("MH-12-1234", "BMW", "White");
+		car2 = new Car("MH-12-1235", "Audi TT", "Goodwood Green Pearl");
+		parkingLot.parkCar(car1);
+		parkingLot.parkCar(car2);
 
 	}
 
 	@Test
 	void testParkCars() {
-		Car car1 = new Car("MH-12-1234", "BMW", "White");
-		Car car2 = new Car("MH-12-1235", "Audi TT", "Goodwood Green Pearl");
+		Car car3 = new Car("MH-12-1236", "Acura TLX Type S PMC Edition", "Gotham Gray");
+		Car car4 = new Car("MH-12-1237", "Bentley’s Mulliner division", "Fine Brodgar Silver");
 
-		parkingLot.parkCar(car1);
-		parkingLot.parkCar(car2);
+		parkingLot.parkCar(car3);
+		parkingLot.parkCar(car4);
 
 		parkingLot.printParkedCars();
 
-		assertEquals(2, parkingLot.getParkedCars().size());
+		assertEquals(4, parkingLot.getParkedCars().size());
 	}
 
+	
+	@Test
+	void testUnparkCars() {
+		parkingLot.unparkCar("MH-12-1234");
+
+        assertFalse(parkingLot.getParkedCars().contains(car1));
+        assertTrue(parkingLot.getParkedCars().contains(car2));
+	}
 }
