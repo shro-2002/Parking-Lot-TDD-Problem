b71efcd7a1456a98a5e6ec4961ae67aa2624a653
shro-2002 <96790181+shro-2002@users.noreply.github.com>
2023-12-24 02:59:41 +0530

    Added Refactored UC-13 and TC-13

--- src/test/java/com/bridgelabz/parkinglottest/ParkingAttendantTest.java
+++ src/test/java/com/bridgelabz/parkinglottest/ParkingAttendantTest.java
@@ -7,36 +7,103 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import com.bridgelabz.enums.CarType;
+import com.bridgelabz.enums.Driver;
 import com.bridgelabz.model.Car;
-import com.bridgelabz.model.CarType;
-import com.bridgelabz.model.Driver;
 import com.bridgelabz.model.ParkingAttendant;
 import com.bridgelabz.model.ParkingLot;
+import com.bridgelabz.model.PoliceDepartment;
 
 class ParkingAttendantTest {
+	PoliceDepartment policeDepartment;
 	ParkingLot parkingLot1;
 	ParkingLot parkingLot2;
 	ParkingAttendant parkingAttendant;
 
+	/*
+	 * @Description - sets up the parking lots and parking attendant
+	 * 
+	 * @param - none
+	 * 
+	 * @return - none
+	 */
 	@BeforeEach
 	void setUp() throws Exception {
 		parkingLot1 = new ParkingLot(5);
 		parkingLot2 = new ParkingLot(3);
 
-		parkingAttendant = new ParkingAttendant(List.of(parkingLot1, parkingLot2));
+		parkingAttendant = new ParkingAttendant(List.of(parkingLot1, parkingLot2), "Grayson");
+		policeDepartment = new PoliceDepartment(parkingAttendant);
 	}
 
+	/*
+	 * @Description - tests the large car parking method
+	 * 
+	 * @param - none
+	 * 
+	 * @return - none
+	 */
 	@Test
-	void test() {
-        System.out.println("Test Direct Large Car");
-        Car largeCar = new Car("WB-12-1238", "Mercedes-Benz GLS", "Black",CarType.LARGE);
+	void testLargeParkingCar() {
+		System.out.println("Test Direct Large Car");
+		Car largeCar = new Car("WB-12-1238", "Mercedes-Benz GLS", "Black", CarType.LARGE);
 
-        parkingAttendant.directLargeCar(largeCar, Driver.Handicapped);
+		parkingAttendant.directLargeCar(largeCar, Driver.Handicapped);
 
-        // Check if the large car is parked in the parking lot with the most free spaces
-        assertTrue(parkingLot1.getParkedCars().contains(largeCar) || parkingLot2.getParkedCars().contains(largeCar));
+		assertTrue(parkingLot1.getParkedCars().contains(largeCar) || parkingLot2.getParkedCars().contains(largeCar));
 
-        System.out.println();
+		System.out.println();
 	}
 
+	/*
+	 * @Description - tests the location of parked cars by color method
+	 * 
+	 * @param - none
+	 * 
+	 * @return - none
+	 */
+	@Test
+	void testGetLocationOfParkedWhiteCars() {
+		System.out.println("Test Get Location of Parked White Cars");
+		Car whiteCar1 = new Car("WB-12-1234", "Toyota", "White", CarType.SMALL);
+		Car whiteCar2 = new Car("WB-12-5678", "Honda", "White", CarType.LARGE);
+
+		parkingLot1.parkCar(whiteCar1, Driver.Handicapped);
+		parkingLot2.parkCar(whiteCar2, Driver.Non_HandiCapped);
+
+		List<String> locations = policeDepartment.getLocationOfParkedColorCars("White");
+
+		for (String location : locations)
+			assertNotNull(location);
+
+		assertEquals(2, locations.size());
+
+		System.out.println();
+	}
+
+	/*
+	 * @Description - tests the location of parked cars by color and brand method
+	 * 
+	 * @param - none
+	 * 
+	 * @return - none
+	 */
+	@Test
+	void testGetLocationOfParkedBlueToyotaCars() {
+		System.out.println("Test Get Location of Parked Blue Toyota Cars");
+		Car blueToyotaCar1 = new Car("WB-12-1234", "Toyota", "Blue", CarType.SMALL);
+		Car blueToyotaCar2 = new Car("WB-12-5678", "Toyota", "Blue", CarType.LARGE);
+
+		parkingLot1.parkCar(blueToyotaCar1, Driver.Handicapped);
+		parkingLot2.parkCar(blueToyotaCar2, Driver.Non_HandiCapped);
+
+		List<String> locations = policeDepartment.getLocationOfParkedColorAndBrandCars("Blue", "Toyota");
+
+		for (String location : locations)
+			assertNotNull(location);
+
+		assertEquals(2, locations.size());
+
+		System.out.println();
+	}
 }
--- src/test/java/com/bridgelabz/parkinglottest/ParkingLotTest.java
+++ src/test/java/com/bridgelabz/parkinglottest/ParkingLotTest.java
@@ -5,9 +5,9 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import com.bridgelabz.enums.CarType;
+import com.bridgelabz.enums.Driver;
 import com.bridgelabz.model.Car;
-import com.bridgelabz.model.CarType;
-import com.bridgelabz.model.Driver;
 import com.bridgelabz.model.ParkingAttendant;
 import com.bridgelabz.model.ParkingLot;
 import com.bridgelabz.observers.AirportSecurity;
@@ -40,19 +40,15 @@ class ParkingLotTest {
 	@BeforeEach
 	void setUp() throws Exception {
 		parkingLot = new ParkingLot(3);// Create a parking lot with capacity 3
-		manager = new ParkingManager();// Create a parking manager
-		airportSecurity = new AirportSecurity();
-		parkingLot.addObservers(manager);// Add the manager to the list of observers
-		parkingLot.addObservers(airportSecurity);// Add the airport security to the list of observers
 
 		// Create two cars
 		System.out.println();
 		car1 = new Car("WB-12-1234", "BMW", "White", CarType.SMALL);
-		car2 = new Car("WB-12-1235", "Audi TT", "Goodwood Green Pearl",CarType.SMALL);
+		car2 = new Car("WB-12-1235", "Audi TT", "Goodwood Green Pearl", CarType.SMALL);
 
 		// Park the cars
-		parkingLot.parkCar(car1,Driver.Handicapped);
-		parkingLot.parkCar(car2,Driver.Non_HandiCapped);
+		parkingLot.parkCar(car1, Driver.Handicapped);
+		parkingLot.parkCar(car2, Driver.Non_HandiCapped);
 
 	}
 
@@ -66,11 +62,11 @@ class ParkingLotTest {
 	@Test
 	void testParkCars() {
 		System.out.println("Test Park Cars");
-		Car car3 = new Car("WB-12-1236", "Acura TLX Type S PMC Edition", "Gotham Gray",CarType.SMALL);
-		Car car4 = new Car("WB-12-1237", "Bentley’s Mulliner division", "Fine Brodgar Silver",CarType.SMALL);
+		Car car3 = new Car("WB-12-1236", "Acura TLX Type S PMC Edition", "Gotham Gray", CarType.SMALL);
+		Car car4 = new Car("WB-12-1237", "Bentley’s Mulliner division", "Fine Brodgar Silver", CarType.SMALL);
 
-		parkingLot.parkCar(car3,Driver.Handicapped);
-		parkingLot.parkCar(car4,Driver.Non_HandiCapped);
+		parkingLot.parkCar(car3, Driver.Handicapped);
+		parkingLot.parkCar(car4, Driver.Non_HandiCapped);
 		assertEquals(3, parkingLot.getParkedCars().size());
 		System.out.println();
 	}
@@ -101,11 +97,17 @@ class ParkingLotTest {
 	 */
 	@Test
 	void testNotifyObservers() {
+		manager = new ParkingManager();
+		airportSecurity = new AirportSecurity();
+		
+		parkingLot.addObservers(manager);
+		parkingLot.addObservers(airportSecurity);
+		
 		System.out.println("Test Notify Observers");
-		Car car3 = new Car("WB-12-1236", "Acura TLX Type S PMC Edition", "Gotham Gray",CarType.SMALL);
-		Car car4 = new Car("WB-12-1237", "Bentley’s Mulliner division", "Fine Brodgar Silver",CarType.SMALL);
+		Car car3 = new Car("WB-12-1236", "Acura TLX Type S PMC Edition", "Gotham Gray", CarType.SMALL);
+		Car car4 = new Car("WB-12-1237", "Bentley’s Mulliner division", "Fine Brodgar Silver", CarType.SMALL);
 
-		parkingLot.parkCar(car3,Driver.Handicapped);
+		parkingLot.parkCar(car3, Driver.Handicapped);
 		parkingLot.parkCar(car4, Driver.Non_HandiCapped);
 
 		assertEquals("full", manager.update(parkingLot.isFull()));
@@ -129,7 +131,7 @@ class ParkingLotTest {
 		Car car4 = new Car("WB-12-1237", "Bentley’s Mulliner division", "Fine Brodgar Silver", CarType.MEDIUM);
 		ParkingAttendant attendant = new ParkingAttendant();
 
-		attendant.parkCar(parkingLot, car3,Driver.Handicapped);
+		attendant.parkCar(parkingLot, car3, Driver.Handicapped);
 		attendant.parkCar(parkingLot, car4, Driver.Non_HandiCapped);
 
 		assertEquals(3, parkingLot.getParkedCars().size());
@@ -161,6 +163,8 @@ class ParkingLotTest {
 	 */
 	@Test
 	void testChargeParking() {
+		manager = new ParkingManager();
+		parkingLot.addObservers(manager);
 		System.out.println("Test Charge Parking");
 
 		parkingLot.parkCar(car2, Driver.Non_HandiCapped);
